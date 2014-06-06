e0fe039dcccdd87c9b0e01a0922477232b93f7d5
alex <argriffi@ncsu.edu>
2014-06-06 15:24:12 -0400

    begin download script

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ p53pipe
 =======
 
 data processing scripts for projects that work with p53 data
+
+http://p53.fr/TP53_database_download/TP53_tumor_database/TP53_US/UMDTP53_curated_2012_R1_US.txt.zip
---
+++ download-disease.py
@@ -0,0 +1,15 @@
+"""
+Check this web page for new versions:
+http://p53.fr/TP53_database_download/TP53_tumor_database/tumor_database.html
+
+If the release has not changed,
+The following text should appear verbatim in the html:
+Current release: June 2012 (2012_R1)
+
+Download the zipped text version of the disease database:
+http://p53.fr/TP53_database_download/TP53_tumor_database/TP53_US/UMDTP53_curated_2012_R1_US.txt.zip
+
+The extracted zip file should contain at least the following file:
+UMDTP53_curated_2012_R1_US.txt
+
+"""
